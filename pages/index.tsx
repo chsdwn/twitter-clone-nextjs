ec46783f06510093bbf2d0d727533fcabc835d93
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-08-10 15:39:31 +0300

    NextJS vs Storybook Kurulumu

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import { Button } from "../components/Button";
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to Next.js!</h1>
+      <Button>Hello</Button>
+    </div>
+  );
+}
+
+export default HomePage;
